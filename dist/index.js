5c357e0fd42068c3481fb082526b5f3e25628180
Arturo Montoya <montoya332@live.com>
2021-08-26 09:44:52 -0700

    update master

--- dist/index.js
+++ dist/index.js
@@ -5903,28 +5903,12 @@ const github = __nccwpck_require__(438);
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  const TENOR_TOKEN = core.getInput('TENOR_TOKEN') || process.env.TENOR_TOKEN;
   const message = core.getInput('message') || 'Thank you!';
   const searchTerm = core.getInput('searchTerm') || 'thank you';
-
-  if ( typeof TENOR_TOKEN !== 'string' ) {
-    throw new Error('Invalid TENOR_TOKEN: did you forget to set it in your action config?');
-  }
 
   if ( typeof GITHUB_TOKEN !== 'string' ) {
     throw new Error('Invalid GITHUB_TOKEN: did you forget to set it in your action config?');
   }
-
-  const randomPos = Math.round(Math.random() * 1000);
-  const url = `https://api.tenor.com/v1/search?q=${encodeURIComponent(searchTerm)}&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high`
-
-  console.log(`Searching Tenor: ${url}`)
-
-  const response = await fetch(`${url}&key=${TENOR_TOKEN}`);
-  const { results } = await response.json();
-  const gifUrl = results[0].media[0].tinygif.url;
-
-  console.log(`Found gif from Tenor: ${gifUrl}`);
 
   const { context = {} } = github;
   const { pull_request } = context.payload;
@@ -5940,11 +5924,12 @@ async function run() {
   await octokit.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: `${message}\n\n<img src="${gifUrl}" alt="${searchTerm}" />`
+    body: `${message}`
   });
 }
 
 run().catch(e => core.setFailed(e.message));
+
 
 /***/ }),
 
